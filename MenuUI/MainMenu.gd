08c760c27473bc317f5db77ac54fefc941a3f609
Pete Thorne <ee0pdt@users.noreply.github.com>
2021-09-20 20:19:44 +0100

    Added audio buses

--- MenuUI/MainMenu.gd
+++ MenuUI/MainMenu.gd
@@ -1,24 +1,10 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_OptionsButton_pressed():
 	get_tree().change_scene("res://MenuUI/Options/Options.tscn")
 
 
+## Handle things like confirmation and saving games here
 func _on_ExitButton_pressed():
 	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
